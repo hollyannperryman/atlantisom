34d9fd3f2ae386ed10f3ffa67f30e7676dae4d7f
Holly Perryman <45412684+hollyannperryman@users.noreply.github.com>
2025-01-31 14:22:41 +0100

    Update load_nc_catchtons.R
    HAP: incorporated the changes I made in order to get the updated CalCurr Atlantis files to run with with updated SS3.

--- R/load_nc_catchtons.R
+++ R/load_nc_catchtons.R
@@ -27,14 +27,23 @@
 #' @keywords gen
 #' @author Alexander Keth, modified by Sarah Gaichas
 #'
+#' HAP - 2024 - The following script was edited by Holly Ann Perryman to get updated CalCurr and SAR SS files to run
 #'
 #' @importFrom magrittr %>%
 #' @export
 
-load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolprm, select_groups,
-  select_variable =
-  c("Catch", "Discard"),
-  check_acronyms = TRUE, bboxes = c(0), verbose = FALSE) {
+load_nc_catchtons <- function(dir = getwd(),
+                              file_nc,
+                              file_fish,
+                              bps,
+                              fgs,
+                              biolprm,
+                              select_groups,
+                              select_variable,
+                              # = c("Catch", "Discard"), # HAP I removed this as I kept getting an error...
+                              check_acronyms = TRUE,
+                              bboxes = c(0),
+                              verbose = FALSE) {
   # NOTE: The extraction procedure may look a bit complex...
   # A different approach would be to
   # create a dataframe for each variable (e.g. GroupAge_Nums)
@@ -53,7 +62,7 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
   }
 
   # Check input of select_variable as only one value is allowed
-  select_variable <- match.arg(select_variable, several.ok = FALSE)
+  select_variable <- match.arg(select_variable, choices = c("Catch", "Discard"), several.ok = FALSE) # HAP - this fixed an error I was encountering
 
   # select_variable gets renamed for fishery data, save input
   input_select_variable <- select_variable
@@ -96,6 +105,9 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
   n_timesteps <- RNetCDF::dim.inq.nc(at_out, 0)$length
   n_boxes     <- RNetCDF::dim.inq.nc(at_out, 1)$length
   n_layers    <- RNetCDF::dim.inq.nc(at_out, 2)$length
+  # layer data
+  num_layers <- RNetCDF::var.get.nc(ncfile = at_out, variable = "numlayers")[, 1]
+  num_layers <- num_layers + ifelse(num_layers == 0, 0, 1)
 
   # Extract data from the ncdf file
   # Create a vector of all potential variable names
@@ -119,7 +131,7 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
     #select_variable <- concatenate select_variable "_" each fleet name
     svfish <- c()
     for(i in select_variable){
-      sv <-paste0(i, "_FC", fleetnames$Index)
+      sv <- paste0(i, "_FC", fleetnames$Index)
       svfish <- c(svfish, sv)
     }
     select_variable <- svfish
@@ -143,22 +155,42 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
   search_clean <- do.call(c, search)
   # If the combination of select_groups and select_variable ends up not being found.
   if (length(search_clean) == 0) return(0)
-
+  # get nc data for search_clean
   at_data <- lapply(search_clean, RNetCDF::var.get.nc, ncfile = at_out)
 
+  #* HAP - to split at_data into agecl data, we need to also get data for FID#_Catch variables
+  search2 <- list()
+  if (input_select_variable %in% "Discard") {input_select_variable<-"Discards"} # Note, the nc var is Discards - not Discard
+  for (i in seq_along(select_groups)) {
+    search2[[i]] <- c(unlist(paste0(fgs[which(fgs$Code %in% select_groups[i]),"Name"], 1:fgs[which(fgs$Code %in% select_groups),"NumCohorts"][i], "_", input_select_variable)))
+    search2[[i]] <- search2[[i]][is.element(search2[[i]], var_names_ncdf)]
+    search2[[i]] <- unique(search2[[i]])
+  }
+  if (input_select_variable %in% "Discards") {input_select_variable<-"Discard"} # swap parameter from Discards to Discard as to presurve the functionality of the rest of the script
+  search_clean2 <- do.call(c, search2)
+  # get nc data for search_clean
+  at_data_agecl <- lapply(search_clean2, RNetCDF::var.get.nc, ncfile = at_out)# NOTE, this data is in NUMBERS
+
   # Get final species and number of ageclasses per species
-  final_species <- select_groups[sapply(
-    lapply(select_groups, grepl, x = search_clean), any)]
+  # final_species <- select_groups[sapply(lapply(select_groups, grepl, x = search_clean), any)] #
+  #* HAP
+  #* in the above, I think there is a bug when setting final_species
+  #* when testing current files from the CalCurr model, this is script is
+  #* selecting FIDs that actually do not have "_Catch_" data being reported out
+  #* from the nc file. I think some of the Codes are flagging multiple groups,
+  #* for example, FPL flags both FPL and PL as being caught but PL is not
+  #* actually caught.
+  #*
+  #* To fix this and get the function to run, I used the following script
+  #* instead of the above
+  # Extract the prefixes from search_clean by splitting at the first underscore
+  final_species <- unique(sapply(strsplit(search_clean, "_"), `[[`, 1))
 
   # # Get final fleets for full age structure fishery output--wrong order
   # if(input_select_variable %in% c("Catch", "Discard")){
   #   final_fleet <- fleetnames$Code[sapply(
   #     lapply(select_variable, grepl, x = search_clean), any)]
   # }
-
-  num_layers <- RNetCDF::var.get.nc(ncfile = at_out, variable = "numlayers")[, 1]
-  # add sediment layer!
-  num_layers <- num_layers + ifelse(num_layers == 0, 0, 1)
 
   # Create an array of layerids.
   # Every entry in the array indicates if a layer is present (= 1) or not (= 0).
@@ -217,9 +249,22 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
   # in 2 subpopulations: 2d-data and 3d-data
   at_data3d <- at_data[which(sapply(at_data, function(x) length(dim(x))) == 3)]
   at_data2d <- at_data[which(sapply(at_data, function(x) length(dim(x))) == 2)]
+  # HAP - not sure if this is necessary but doing the same for the at_data_agecl data
+  at_data2d_agecl <- at_data_agecl[which(sapply(at_data_agecl, function(x) length(dim(x))) == 2)]
 
   int_fs <- final_species
-  int_fa <- rep(1, length(final_species))
+  #int_fa <- rep(1, length(final_species))
+  #* HAP -
+  #* In my work, I realized I am not getting agecl data from this in that all the values in agecl
+  #* in the resulting datasets were 1. First, I tried swapping the 1 in the above for int_fa with 10 and
+  #* it appeared to work (ie the function runs) but catchtons data no longer matched catch.txt data
+  #* After a bit more digging this is when I realized that the data in the nc file being used here does
+  #* not have agecl data. The dime of FID_Catch_FC# is boxes:time.
+  #* However, nc data FID#_Catch does of agecl data, and is also as dim boxes_time.
+  #* This is when I decided to adjust this script to collect this data to compute proportions for
+  #* splitting FID_Catch_FC# into agecl.
+  #* To make sure we are getting the correct max age, I used the following instead for int_fa
+  int_fa <- fgs[which(fgs$Code %in% final_species),"NumCohorts"]
 
   if(input_select_variable %in% c("Catch", "Discard")){
     # how many unique fleets per select_group in search_clean?
@@ -286,13 +331,25 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
     # # age-structured invert groups are combined in ncdf file!
     # if (input_select_variable == "Grazing") int_fa <- 1
 
-    for (i in seq_along(at_data2d)) {# for loop over all variables!
-      if (i == 1) result2d <- list()
-      for (j in 1:n_timesteps) {# loop over timesteps
-        if (j == 1) values <- array(dim = c(length(boxes), n_timesteps))
-        values[, j] <- at_data2d[[i]][, j][boxes + 1]
+    #* HAP - this for loop was modified in order to use at_data2d_agecl data to split at_data2d by agecl
+    index <- rep(1:length(int_fs), each = int_ff[1])
+    for (i in seq_along(at_data2d)) {# for loop over all variables! i=a=j=1
+      if (i == 1) result2d <- list() # define an empty list to insert vectorized data
+      # for variable i, use at_data2d_agecl data to split at_data2d by agecl
+      if (i <= length(fleetnames$Index)) {proportions_agecl_s <- lapply(at_data2d_agecl[c(1:int_fa[index[i]])], function(x) x / Reduce("+", at_data2d_agecl[c(1:int_fa[index[i]])]))
+      } else {proportions_agecl_s <- lapply(at_data2d_agecl[c(int_fa[index[i]-1]+(c(1:int_fa[index[i]])))], function(x) x / Reduce("+", at_data2d_agecl[c(int_fa[index[i]-1]+(c(1:int_fa[index[i]])))]))
       }
-      result2d[[i]] <- as.vector(values)
+      scaled_proportions_i <- lapply(proportions_agecl_s, function(x) {
+        x[is.nan(x)] <- 0  # Replace NaN with 0
+        x * at_data2d[[i]]
+      })
+      for (a in seq_along(scaled_proportions_i)) {
+        if (a == 1) values2 <- array(dim = c(length(scaled_proportions_i), length(boxes), n_timesteps)) # define an empty array
+        for (j in 1:n_timesteps) {# loop over timesteps
+          values2[a, , j] <- scaled_proportions_i[[a]][, j][boxes + 1] # select data for element i and time j, dropping boxes that are not in model domain, and insert into defined array
+        }
+      }
+      result2d[[i]] <- as.vector(values2)
     }
 
     # Order of the data in value column = "atoutput".
@@ -326,21 +383,39 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
 
     #should now work properly for multiple fleets
     if(input_select_variable %in% c("Catch", "Discard")){
-      result2d <- data.frame(species = unlist(sapply(X = mapply(FUN = rep, x = int_fs,
-                   each = (int_fa * int_ff),SIMPLIFY = FALSE,USE.NAMES = FALSE),
-        FUN = rep, each = length(boxes) * n_timesteps, simplify = FALSE)),
-        agecl = unlist(sapply(X = sapply(X = rep(int_fa, int_ff), FUN = seq, from = 1,
-                                         by = 1, simplify = FALSE, USE.NAMES = FALSE),
-                              FUN = rep, each = (length(boxes) * n_timesteps), simplify = FALSE)),
-        polygon = unlist(sapply(X = n_timesteps * int_fa * int_ff,
-                                FUN = rep, x = boxes, simplify = FALSE, USE.NAMES = FALSE)),
-        fleet = unlist(sapply(X = mapply(FUN = rep, x = sp_fleet$fleet,
-                                         each = (rep(int_fa, int_ff)),SIMPLIFY = FALSE,USE.NAMES = FALSE),
-          FUN = rep, each = (length(boxes) * n_timesteps), simplify = FALSE)),
-        time = unlist(sapply(X = int_fa * int_ff , FUN = rep, x = rep(0:(n_timesteps - 1),
-                                                            each = length(boxes)), simplify = FALSE, USE.NAMES = FALSE)),
-        atoutput = do.call(c, result2d),
-        stringsAsFactors = F)
+      # result2d <- data.frame(species = unlist(sapply(X = mapply(FUN = rep, x = int_fs,
+      #              each = (int_fa * int_ff),SIMPLIFY = FALSE,USE.NAMES = FALSE),
+      #   FUN = rep, each = length(boxes) * n_timesteps, simplify = FALSE)),
+      #   agecl = unlist(sapply(X = sapply(X = rep(int_fa, int_ff), FUN = seq, from = 1,
+      #                                    by = 1, simplify = FALSE, USE.NAMES = FALSE),
+      #                         FUN = rep, each = (length(boxes) * n_timesteps), simplify = FALSE)),
+      #   polygon = unlist(sapply(X = n_timesteps * int_fa * int_ff,
+      #                           FUN = rep, x = boxes, simplify = FALSE, USE.NAMES = FALSE)),
+      #   fleet = unlist(sapply(X = mapply(FUN = rep, x = sp_fleet$fleet,
+      #                                    each = (rep(int_fa, int_ff)),SIMPLIFY = FALSE,USE.NAMES = FALSE),
+      #     FUN = rep, each = (length(boxes) * n_timesteps), simplify = FALSE)),
+      #   time = unlist(sapply(X = int_fa * int_ff , FUN = rep, x = rep(0:(n_timesteps - 1),
+      #                                                       each = length(boxes)), simplify = FALSE, USE.NAMES = FALSE)),
+      #   atoutput = do.call(c, result2d),
+      #   stringsAsFactors = F)
+      #* HAP:
+      #* above is the old way of preparing function output. This had to be adjusted to account for the agecl data
+      #* With the agecl data being integrated, the vectorization of the data changed, making the above store the
+      #* result2d incorrectly. Below now properly stores the vectorized data, well it does with my case study.
+      #* of course folks should test and check to make sure this works for them too...
+      result2d <- data.frame(species = unlist(Map(function(x, reps) rep(x, reps), int_fs, int_ff * n_timesteps * length(boxes) * int_fa), use.names = FALSE),
+                             agecl   = unlist(sapply(X = rep(int_fa),
+                                                     FUN = function(x) rep(seq(from = 1, by = 1, length.out = 10), times = (length(boxes) * n_timesteps * int_ff[1])),
+                                                     simplify = FALSE, USE.NAMES = FALSE)),
+                             polygon = unlist(lapply(lapply(int_fa, function(n) rep(boxes, each = n)),
+                                                     function(x) rep(x, times = n_timesteps * int_ff[1]))),
+                             fleet   = unlist(Map(function(seq_item, reps) rep(seq_item, each = reps),
+                                                  lapply(int_ff, function(x) seq(1, x)),  int_fa * length(boxes) * n_timesteps)),
+                             time    =  unlist(lapply(lapply(int_fa * length(boxes), function(n) rep(0:(n_timesteps - 1), each = n)),
+                                                      function(x) rep(x, times = int_ff[1]))),
+                             atoutput = do.call(c, result2d),
+                             stringsAsFactors = F)
+
       #if (select_variable == "N") result2d$layer <- n_layers - 1
     }
 
@@ -350,14 +425,15 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
   # if (all(sapply(lapply(at_data, dim), length) == 3) & input_select_variable != "N") {
   #   result <- result3d
   # }
+  # HAP - I changed this in an attempt to reduce the use of background storage
   if (all(sapply(lapply(at_data, dim), length) == 2) & input_select_variable != "N") {
-    result <- result2d
-  }
-  if (input_select_variable == "N") {
+    return(result2d)
+  } else if (input_select_variable == "N") {
     if (length(at_data2d) >= 1 & length(at_data3d) == 0) result <- result2d
     if (length(at_data2d) == 0 & length(at_data3d) >= 1) result <- result3d
     if (length(at_data2d) >= 1 & length(at_data3d) >= 1) {
       result <- rbind(result2d, result3d)
+      return(result)
     }
   }
 
@@ -392,5 +468,5 @@ load_nc_catchtons <- function(dir = getwd(), file_nc, file_fish, bps, fgs, biolp
   #     dplyr::summarise(atoutput = sum(atoutput))
   # }
 
-  return(result)
+  # return(result)
 }
