0bbf1a458938b3cd63f4ed14aa08911993a0d1c4
Holly Perryman <45412684+hollyannperryman@users.noreply.github.com>
2025-01-31 14:27:18 +0100

    Update SS_write_comps.R
    I incorporated the changes I needed to make in order to get the updated CalCurr Atlantis files to run with the updated SS3.

--- R/SS_write_comps.R
+++ R/SS_write_comps.R
@@ -9,20 +9,28 @@
 #'@param bins a list item, each item is a vector specifying either the age or length bins
 #'@param caal_bool a vector of length \code{ss_data_list} with boolean values for whether the data is conditional age-at-length
 #'@return modified ss_data_list that includes added composition data
-SS_write_comps <- function(ss_data_list, comp_matrix,
-                           data_rows, sampling_month,
-                           data_type, fleet_number,
-                           bins, caal_bool){
+
+#' HAP - 2024 - The following script was edited by Holly Ann Perryman to get updated CalCurr and SAR SS files to run
+
+SS_write_comps <- function(ss_data_list,
+                           comp_matrix,
+                           data_rows,
+                           sampling_month,
+                           data_type,
+                           fleet_number,
+                           bins,
+                           caal_bool){
 
   #Check if age and length comp are in the data_type vector and remove all rows if so
   if("agecomp" %in% data_type){
-    ss_data_list$agecomp <- ss_data_list$agecomp[0,]
+    # HAP - changed this to only take the first 9 columns and thus dropping all a# columns to account for age bins in atlantis differing from the dat file
+    ss_data_list$agecomp <- ss_data_list$agecomp[0,c(1:9)]
     age_comp_ind <- which(data_type=="agecomp")
   }
 
   if("lencomp" %in% data_type){
-
-    ss_data_list$lencomp <- ss_data_list$lencomp[0,c("Yr","Seas","FltSvy","Gender","Part","Nsamp")]
+    # HAP - new column names
+    ss_data_list$lencomp <- ss_data_list$lencomp[0,c("year","month","fleet","sex","part","Nsamp")] # c("Yr","Seas","FltSvy","Gender","Part","Nsamp") # month
     len_comp_ind <- which(data_type=="lencomp")
   }
 
@@ -34,52 +42,46 @@ SS_write_comps <- function(ss_data_list, comp_matrix,
     # if(i==3){
     #   browser()
     # }
-    k <- switch(data_type[i],"lencomp"=k_lencomp,
-                "agecomp"=k_agecomp)
+    k <- switch(data_type[i], "lencomp"=k_lencomp, "agecomp"=k_agecomp)
+    # HAP - new column names
     indices <- (k:(k+length(data_rows[[i]])-1))
-    ss_data_list[[data_type[i]]][indices,"Yr"] <- data_rows[[i]]
-    ss_data_list[[data_type[i]]][indices,"Seas"] <- sampling_month[[i]]
-    ss_data_list[[data_type[i]]][indices,"FltSvy"] <- rep(fleet_number[i], length(data_rows[[i]]))
-
-    ss_data_list[[data_type[i]]][indices,"Gender"] <-
-      rep(0, length(data_rows[[i]]))
-
-    ss_data_list[[data_type[i]]][indices,"Part"] <-
-      rep(0,length(data_rows[[i]]))
-
-    ss_data_list[[data_type[i]]][indices,"Nsamp"] <-
-      comp_matrix[[i]][,"nsamp"]
-
-    type_prefix <- switch(data_type[i],
-                          "agecomp"="a",
-                          "lencomp"="l")
-
+    ss_data_list[[data_type[i]]][indices,"year"]  <- data_rows[[i]]
+    ss_data_list[[data_type[i]]][indices,"month"] <- sampling_month[[i]]
+    ss_data_list[[data_type[i]]][indices,"fleet"] <- rep(fleet_number[i], length(data_rows[[i]]))
+    ss_data_list[[data_type[i]]][indices,"sex"]   <- rep(0, length(data_rows[[i]]))
+    ss_data_list[[data_type[i]]][indices,"part"]  <- rep(0,length(data_rows[[i]]))
+    ss_data_list[[data_type[i]]][indices,"Nsamp"] <- comp_matrix[[i]][,"nsamp"]
+    type_prefix <- switch(data_type[i],"agecomp"="a","lencomp"="l")
 
     if(data_type[i]=="agecomp"){
       if(caal_bool[i]){
         ss_data_list[[data_type[i]]][indices,"Lbin_lo"] <- comp_matrix[[i]][,"lower.bins"]
         ss_data_list[[data_type[i]]][indices,"Lbin_hi"] <- comp_matrix[[i]][,"upper.bins"]
       } else{
-      ss_data_list[[data_type[i]]][indices,"Lbin_lo"] <- rep(-1,length(data_rows[[i]]))
-      ss_data_list[[data_type[i]]][indices,"Lbin_hi"] <- rep(-1,length(data_rows[[i]]))
+        ss_data_list[[data_type[i]]][indices,"Lbin_lo"] <- rep(-1,length(data_rows[[i]]))
+        ss_data_list[[data_type[i]]][indices,"Lbin_hi"] <- rep(-1,length(data_rows[[i]]))
       }
-      ss_data_list[[data_type[i]]][indices, "Ageerr"] <- rep(1,length(data_rows[[i]]))
+      ss_data_list[[data_type[i]]][indices, "ageerr"] <- rep(1,length(data_rows[[i]])) # HAP - new column name
     }
 
-  if(data_type[[i]]=="lencomp"){
-    if(indices[1]==1){
-     ss_data_list[[data_type[i]]][, paste(type_prefix,as.character(bins[[i]]), sep="")] <- 0
+    if(data_type[[i]]=="lencomp"){
+      if(indices[1]==1){
+        ss_data_list[[data_type[i]]][, paste(type_prefix,as.character(bins[[i]]), sep="")] <- 0
+      }
     }
-  }
-    ss_data_list[[data_type[i]]][indices,paste(type_prefix,as.character(bins[[i]]), sep="")] <- comp_matrix[[i]][,as.character(bins[[i]])]
+    # HAP - I had to edit the following line as it was making lencomp data char, rather than num or int, which was giving me issues
+    ss_data_list[[data_type[i]]][indices,paste(type_prefix,as.character(bins[[i]]), sep="")] <- comp_matrix[[i]][,as.character(bins[[i]]), with = FALSE]
+
+    # HAP - have to check for NAs, likely due to mismatching bins across fleets, and turn NAs to zeros
+    ss_data_list[[data_type[i]]][is.na(ss_data_list[[data_type[i]]])] <- 0
+
     if(data_type[i]=="agecomp"){
       k_agecomp <- k_agecomp+length(data_rows[[i]])
     } else{
-
       k_lencomp <- k_lencomp+length(data_rows[[i]])
     }
-    }
-    return(ss_data_list)
-
   }
+
+  return(ss_data_list)
+}
 
